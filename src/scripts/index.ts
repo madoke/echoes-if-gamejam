02b2d8fc271f0740c13a7359088d2d661ec109ac
David Simao <madoke@gmail.com>
2024-06-16 17:47:22 +0100

    initial commit

---
+++ src/scripts/index.ts
@@ -0,0 +1,5 @@
+import thecase from "./1_case.narrat";
+import investigation from "./2_investigation.narrat";
+import revelations from "./3_revelations.narrat";
+
+export default [thecase, investigation, revelations];
